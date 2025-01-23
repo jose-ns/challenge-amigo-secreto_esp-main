ce789e328d636bcb6e064d7cc0f2e753a45f59ab
josens <jose.neira.sandoval@gmail.com>
2025-01-23 16:32:35 -0300

    actualiza el js, css y html

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function agregarAmigo(){
     }else{
         amigos.push(nombre);
         limpiarCaja();
+        actualizaLista();
     }
     return;
 }
@@ -26,22 +27,31 @@ function actualizaLista(){
     //definir variable para el ul
     let lista = document.getElementById('listaAmigos');
     //lImpiar la lista
-    lista.innerHTML = '';
+    lista.innerHTML = "";
     //recorrer el array amigos
-    for(let i = 0; i < amigos.length; i++);{
+    for(let i = 0; i < amigos.length; i++){
         //definir li y crear elementos li
-        let li = document.createElement("li");
+        let nuevoLi = document.createElement("li");
         //contenido de li
-        li.textContent = amigos[i];
+        nuevoLi.textContent = amigos[i];
         //agregar li a la lista ul
-        lista.appendChild(li);
+        lista.appendChild(nuevoLi);
+    } 
+    if(amigos.length > 0){
+        lista.classList.remove('oculta');
+    } else {
+        lista.classList.add('oculta');
     }
-    }
+}
 
-    // function sortearAmigo(){
-    //     amigos.length;
-    //     //sortear amigo
-    //     let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
-    //     //mostrar el amigo sorteado
-    //     document.getElementById('amigoSorteado').textContent = amigoSorteado;
-    // }
+
+    function sortearAmigo(){
+        if (amigos.length === 0){
+            alert('No hay amigos disponibles para el sorteo.');
+            return;
+        }
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+        document.getElementById('resultado').innerHTML = amigoSorteado;
+        document.getElementById('listaAmigos').classList.add('oculta');
+    }
